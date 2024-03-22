c5daea54140053c274e92e031c663b187befca17
Maxxxxxx-x <maxxng1028@gmail.com>
2024-03-22 18:10:41 +0800

    feat: added checks for bun and clear flag dependent on env

--- start.sh
+++ start.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+declare -r environment="development"
+
+# check for bun
+if ! command -v bun &> /dev/null; then
+    echo "Bun not found, installing Bun"
+    if [ "$OSTYPE" == "linux-gnu"* ] || [ "$OSTYPE" == "darwin"* ]; then
+        curl -fsSL https://bun.sh/install | bash
+    else
+        powershell -c "irm bun.sh/install.ps1|iex"
+    fi
+fi
+
+if [ "$1" == "--clear" ] && [ environment == "development" ]; then
+    echo "Deleting config.json"
+    rm ./config.json
+fi
+
+bun install
 
 bun run ./src/main.ts
