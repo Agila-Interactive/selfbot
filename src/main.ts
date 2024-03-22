1573e4459ed39046302747f7ed0965099efd549a
Maxxxxxx-x <maxxng1028@gmail.com>
2024-03-22 11:47:26 +0800

    feat: added initConfig and start

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,27 @@
 import { Client } from "discord.js-selfbot-v13";
 
+
 import { loadEventListeners } from "./modules/loadEventListener";
 import { prompt } from "./modules/prompter"
 import type { Config } from "./types/Config";
-
-async function setup() : Promise<Config> {
-
-}
-
-
+import { initConfig } from "./modules/setup";
+import { join} from "node:path";
 
 async function botLogin() : Promise<Client> {
-
-
     const token = await prompt({ question: "Enter your discord token: ", type: "string" });
     const client = new Client();
     await loadEventListeners(client);
 
 }
 
-botLogin();
+async function showMenu() : Promise<void> {
+    const logo = Bun.file(join(import.meta.dir, "templates", "logo.txt"));
+    console.log(await logo.text())
+}
+
+async function start() {
+    showMenu();
+}
+
+await initConfig();
+await start();
