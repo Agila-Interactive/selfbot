29c2fc1f4d45a6c628e405e9fcaef9e2c12234dc
maxxxxx <maxxng1028@gmail.com>
2024-03-30 04:00:59 +0800

    feat: completed getUser with bot and self filter

--- src/modules/getUserIds.ts
+++ src/modules/getUserIds.ts
@@ -1,12 +1,28 @@
-import type { Client, Guild, Message, GuildMember } from "discord.js-selfbot-v13";
+import type { Client, Guild, Message } from "discord.js-selfbot-v13";
 
-export async function getUserIds(message : Message, client : Client) : Promise<void > {
-    const guildId : string = message.guildId!;
-    const guild : Guild = await client.guilds.fetch(guildId);
+import { sendToPackwatch } from "./sendToPackwatch";
+import { getSavedConfig } from "./configHandler";
+import { writeToFile } from "./writeToFile";
+import { sendToBackup } from "./sendBackup";
+import { exitGrace } from "./utils";
+
+export async function getUserIds(message: Message, client: Client): Promise<void> {
+    const guildId: string = message.guildId!;
+    const guild: Guild = await client.guilds.fetch(guildId);
     const totalMemberCount = guild.memberCount;
-    const members = await guild.members.fetch();
+    let members = await guild.members.fetch();
     const fetched = guild.members.cache.size;
     console.log(`Total number of members: ${totalMemberCount}\nNumber of users fetched successfully: ${fetched}\nPercentage: ${(fetched / totalMemberCount) * 100}%`);
-
+    members = members.filter(member => !member.user.bot && member.id !== client.user!.id);
+    const config = await getSavedConfig();
+    if (config === null) {
+        console.log("Failed to read config. Aborting...");
+        exitGrace(client);
+    }
+    const timestamp = Date.now();
+    const fileName = `${timestamp}_${guildId}.txt`;
+    await writeToFile(client, config!, fileName, members);
+    await sendToBackup(config!, client, fileName);
+    await sendToPackwatch(config!, client, fileName);
+    return;
 }
-
