b2c625ac452209794a3b6197c05bed36f6ca9afd
maxxxxx <maxxng1028@gmail.com>
2024-03-30 04:05:18 +0800

    feat: added exitGrace

--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -1,14 +1,24 @@
-import { statSync} from "node:fs";
+import type { Client } from "discord.js-selfbot-v13";
+import { statSync } from "node:fs";
 
 export function validatePath(path: string, isDir = false): boolean {
     try {
-        const status  = statSync(path);
+        const status = statSync(path);
         return ((status.isFile() && !isDir) || (status.isDirectory() && isDir))
-    } catch(error) {
+    } catch (error) {
         // @ts-ignore
         if (error.code === "ENOENT") {
             console.log("Provided path does not exist!");
         }
         return false;
     }
+}
+
+export async function exitGrace(client: Client) {
+    if (client !== undefined) {
+        console.log(`Logging out from ${client.user!.username}`);
+        client.logout();
+    }
+    console.log("Goodbye!");
+    process.exit();
 }
